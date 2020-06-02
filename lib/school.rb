2d48c9c22d4284bb8340bc655811c2246c5158fe
charlie763 <cwisoff@gmail.com>
2020-06-02 12:45:41 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+	attr_reader :roster, :name
+
+	def initialize(name)
+		@roster = {}
+		@name = name
+	end
+
+	def add_student(name, grade)
+		@roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
+	end
+
+	def grade(grade)
+		@roster[grade]
+	end
+
+	def sort
+		# sorted_roster = {}
+		# @roster.each do |grade, name_list|
+		# 	sorted_roster[grade] = name_list.sort
+		# end
+		# sorted_roster
+
+		@roster.map{|grade_names| [grade_names[0], grade_names[1].sort]}.to_h
+	end
+end
