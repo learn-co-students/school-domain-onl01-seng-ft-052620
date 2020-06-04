41195460c4165c54083e9146ada52204c1eb44eb
Nroulston <nicolas.roulston@gmail.com>
2020-06-04 13:32:36 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+require "pry"
+
+class School
+    
+    def initialize(school)
+        @school = school
+        @roster = {}
+        
+    end
+    def roster 
+        @roster
+    end
+
+    def add_student(name, grade)
+        unless @roster.has_key?(grade)
+          @roster[grade] = []
+        end
+        @roster[grade] << name
+    end
+    
+    def grade(number)
+        @roster[number]
+    end
+
+    def sort 
+         @roster.keys.sort.map! do |i|
+             @roster[i] = @roster[i].sort
+         end
+        @roster
+    end
+end
