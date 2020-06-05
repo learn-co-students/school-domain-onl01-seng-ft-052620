830980420ce28f1a18d4dacbd1d65276f874a72e
Eyerusalem K <3yerus@gmail.com>
2020-06-05 07:54:40 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require "pry"
+
+class School
+  attr_accessor :name, :roster
+  attr_writer :grade
+
+
+def initialize(name)
+@name = name
+@roster = {}
+end
+
+def add_student(name, grade)
+  # binding.pry
+  @roster[grade] ||= []
+  @roster[grade] << name
+end
+
+
+def grade(num)
+  @roster[num]
+end
+
+def sort
+  new_hash= {}
+  
+  roster.keys.sort.each do |grade|
+    new_hash[grade] = roster[grade].sort
+   end 
+ new_hash
+ end 
+end 
+
+school = School.new("Bayside High School") 
