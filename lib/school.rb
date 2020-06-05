8a160821260197c9f3a98ccdaf81edc93a805c59
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-05 19:32:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+attr_accessor :name, :roster
+  
+def initialize(name)
+    @name = name
+    @roster = {}
+end
+  
+def add_student(name,grade)
+  if self.roster.include?(grade)
+      self.roster[grade] << name
+ else 
+      self.roster[grade] = [name]
+  end
+end
+
+def grade(grade)
+  roster[grade]
+end
+
+def sort
+  roster.each do |grade|
+    grade[1].sort!
+  end
+
+roster
+end
+
+end
