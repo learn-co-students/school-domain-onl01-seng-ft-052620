74e0d2f774e1f8032e6a9547a40ab17bca2f4f2f
ejguzman3988 <eriberto176@gmail.com>
2020-06-06 03:01:17 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
 # code here!
+
+
+class School
+
+    
+
+    def initialize(name)
+        @name = name
+        @roster = Hash.new
+    end
+
+    def add_student(name, grade)
+        if @roster.include?(grade)
+            @roster[grade] << name
+        else
+                
+            @roster[grade] = []
+            @roster[grade] << name
+        end
+    end
+
+    def roster
+        @roster
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        
+        roster.each_key do |grade|
+            
+            roster[grade] = roster[grade].sort
+            
+        end
+        roster
+        
+        
+        
+    end
+end
